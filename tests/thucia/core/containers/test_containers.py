90a971b2b0acd6ca423c8078b14480d71cce5b9e
jsbrittain <98161205+jsbrittain@users.noreply.github.com>
2025-06-20 09:16:57 +0100

    Fix docker test

--- tests/thucia/core/containers/test_containers.py
+++ tests/thucia/core/containers/test_containers.py
@@ -1,17 +1,20 @@
 from pathlib import Path
-from thucia.core.containers import build_container, run_in_container
+
+from thucia.core.containers import build_container
+from thucia.core.containers import run_in_container
+
 
 def test_containers():
     build_container(
-        path=Path(__file__).parent,
+        path=str(Path(__file__).parent),
         tag="thucia-test/alpine:latest",
         dockerfile="Dockerfile",
-        remove=True
+        remove=True,
     )
     result, output = run_in_container(
         image="thucia-test/alpine:latest",
         command=["sh", "-c", "echo hello"],
-        volumes={}
+        volumes={},
     )
     assert result == 0
-    assert output == 'hello'
+    assert output.strip() == "hello"
