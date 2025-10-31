953153af39da83e11e0e89510265eecf6896d10a
jsbrittain <98161205+jsbrittain@users.noreply.github.com>
2025-10-31 14:42:55 +0000

    Baseline model performs on-the-fly quantile conversion to preserve memory

--- src/thucia/core/models/baseline.py
+++ src/thucia/core/models/baseline.py
@@ -6,6 +6,9 @@ import numpy as np
 import pandas as pd
 from thucia.core.cases import align_date_types
 from thucia.core.fs import DataFrame
+from thucia.core.models.utils import sample_to_quantiles_vec
+
+from .utils import quantiles
 
 
 class BaselineSamples:
@@ -130,12 +133,12 @@ def baseline(
     for gid2 in df["GID_2"].unique():
         logging.info(f"Processing region {gid2} for baseline model")
         # Filter data for the current region
-        region_data = df[df["GID_2"] == gid2].set_index("Date")
+        region_data = df[df["GID_2"] == gid2].set_index("Date").sort_index()
 
         # Calculate incidence differences
         inc_diffs = region_data["Cases"].diff().fillna(0)
 
-        df_samples = []
+        df_quantiles = []
         for step_ahead in range(1, horizon + 1):
             logging.info(f"Processing horizon: {step_ahead}")
 
@@ -149,12 +152,14 @@ def baseline(
                     symmetrize=symmetrize,
                 )
 
-                # If the previous case was a 'future' (prediction), use the median estimate
-                # from the last level, otherwise use the actual case count
+                # If the previous case was a 'future' (prediction), use the median
+                # estimate from the last level, otherwise use the actual case count
                 if region_data["future"].iloc[k - step_ahead]:
                     last_level = (
-                        np.nanmedian(df_samples[-step_ahead]["prediction"])
-                        if df_samples
+                        df_quantiles[-step_ahead][
+                            df_quantiles[-step_ahead]["quantile"] == 0.5
+                        ]["prediction"].values[0]
+                        if df_quantiles
                         else 0
                     )
                 else:
@@ -170,23 +175,23 @@ def baseline(
                 )[:, -1]  # ...taking only the furthest point as the n-step predictor
                 predictions = np.clip(predictions, 0, None)
 
-                df_samples.append(
+                s2q = sample_to_quantiles_vec(predictions, quantiles)
+                df_quantiles.append(
                     pd.DataFrame(
                         {
-                            "GID_2": [gid2] * num_samples,
-                            "Date": [region_data.index[k]] * num_samples,
-                            "sample": list(range(num_samples)),
-                            "prediction": predictions.tolist(),
-                            "horizon": [step_ahead] * num_samples,
-                            "Cases": region_data["Cases"].iloc[k],  # actual case count
+                            "GID_2": [gid2] * len(quantiles),
+                            "Date": [region_data.index[k]] * len(quantiles),
+                            "quantile": s2q["quantile"],
+                            "prediction": s2q["value"],
+                            "horizon": [step_ahead] * len(quantiles),
                         }
                     )
                 )
-        df_predictions = pd.concat(df_samples, ignore_index=True)
+        df_predictions = pd.concat(df_quantiles, ignore_index=True)
         tdf.append(
             df_predictions.merge(
                 df[["GID_2", "Date", "Cases", "future"]],
-                on=["GID_2", "Date", "Cases"],
+                on=["GID_2", "Date"],
                 how="right",
             )
         )
